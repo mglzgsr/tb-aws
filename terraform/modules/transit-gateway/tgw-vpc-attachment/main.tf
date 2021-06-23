d7c5e8ab0dab547b5dbb31416e776e17c62020a3
Miguel <mlii@gft.com>
2021-06-23 14:58:44 +0100

    commenting count lines

--- terraform/modules/transit-gateway/tgw-vpc-attachment/main.tf
+++ terraform/modules/transit-gateway/tgw-vpc-attachment/main.tf
@@ -1,6 +1,6 @@
 #Attach a VPC to TGW
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_vpc_attachment" {
-  count              = length(var.transit_gateway_id) > 0 ? 1 : 0
+  # count              = length(var.transit_gateway_id) > 0 ? 1 : 0
   subnet_ids         = var.subnets_ids
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = var.vpc_id
